8ef01f3ddee7e694496661aa421b392c893bc70b
rodrigo0151 <rodrig.pereirasilva15@gmail.com>
2025-09-24 21:13:49 -0300

    Add files via upload

---
+++ 4divisaodosnumeros.c
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main(void)
+{
+    int n[2],divs;
+    int i;
+    divs=0;
+    for (i=0;i<2;i++)
+    {
+        printf("\n Digite o primeiro número %d :", i);
+        scanf("%d",&n[i]);
+   
+    if (divs=n[0]/n[1]);
+     else if (divs=n[1]/n[0]);
+    }
+    printf("\n A divisão foi :%d", divs);
+}
